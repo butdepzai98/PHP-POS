5bb324a8779884b680780d96ebf67d0c34a38864
Xuan Vinh <winxuanvinh98@gmail.com>
2020-07-21 11:29:12 +0700

    create Category

--- models/Category.php
+++ models/Category.php
@@ -4,13 +4,5 @@ require "connection.php";
 
 class Category
 {
-    public function ShowCategory()
-    {
-        # code...
-    }
-
-    public function createCategory()
-    {
-        # code...
-    }
+    
 }
