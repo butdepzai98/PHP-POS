67829bf7066e1613211e17bc3c233ec3136ad4a3
Xuan Vinh <winxuanvinh98@gmail.com>
2020-07-21 10:04:11 +0700

    Update Model Category

--- models/Category.php
+++ models/Category.php
@@ -4,5 +4,13 @@ require "connection.php";
 
 class Category
 {
-    
+    public function ShowCategory()
+    {
+        # code...
+    }
+
+    public function createCategory()
+    {
+        # code...
+    }
 }
